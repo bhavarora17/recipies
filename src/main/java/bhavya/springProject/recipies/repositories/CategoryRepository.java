6b7ea1d94afe0995f3eeb0485545d804f3ce803e
SEA\barora <barora@egencia.com>
2018-01-10 01:22:42 -0800

    adding repositories

--- src/main/java/bhavya/springProject/recipies/repositories/CategoryRepository.java
+++ src/main/java/bhavya/springProject/recipies/repositories/CategoryRepository.java
@@ -1,11 +1,10 @@
 package bhavya.springProject.recipies.repositories;
 
-import bhavya.springProject.recipies.domain.Recipe;
+import bhavya.springProject.recipies.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by barora on 1/10/2018.
  */
-public interface RecipeRepositories extends CrudRepository<Recipe, Long>{
-
+public interface CategoryRepository extends CrudRepository<Category, Long> {
 }
--- src/main/java/bhavya/springProject/recipies/repositories/UnitOfMeasureRespository.java
+++ src/main/java/bhavya/springProject/recipies/repositories/UnitOfMeasureRespository.java
@@ -1,11 +1,9 @@
 package bhavya.springProject.recipies.repositories;
 
-import bhavya.springProject.recipies.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by barora on 1/10/2018.
  */
-public interface RecipeRepositories extends CrudRepository<Recipe, Long>{
-
+public interface UnitOfMeasureRespository extends CrudRepository<UnitOfMeasureRespository, Long> {
 }
